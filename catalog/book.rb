bb55425912781ac2fb70f56b47672373a02ee18b
mumo-esther <mumoesther2021@gmail.com>
2023-05-31 22:58:36 +0300

    Changes on book.rb

--- catalog/book.rb
+++ catalog/book.rb
@@ -1,3 +1,5 @@
+require 'json'
+
 class Book
   attr_accessor :title, :author, :publisher
 
@@ -7,6 +9,34 @@ class Book
   end
 
   def can_be_archived?
-    super || @cover_state == 'bad'
+    @cover_state == 'bad'
+  end
+
+  def self.load_collection(file_path)
+    json_data = File.read(file_path)
+    books_data = JSON.parse(json_data, symbolize_names: true)
+    books_data.map { |book_data| from_json(book_data) }
+  end
+
+  def self.save_collection(books, file_path)
+    books_data = books.map(&:to_json)
+    json_data = JSON.generate(books_data)
+    File.write(file_path, json_data)
+  end
+
+  def to_json
+    {
+      title: @title,
+      author: @author,
+      publisher: @publisher,
+      cover_state: @cover_state
+    }
+  end
+
+  def self.from_json(data)
+    new(data[:cover_state], data[:publisher]).tap do |book|
+      book.title = data[:title]
+      book.author = data[:author]
+    end
   end
 end
