ad57c76bdd7e008b6f51ab08565fd782493af3bd
mumo-esther <mumoesther2021@gmail.com>
2023-05-31 16:52:39 +0300

    Fix Linter errors

--- catalog/author.rb
+++ catalog/author.rb
@@ -1,17 +1,17 @@
 class Author
-attr_reader :items
+  attr_reader :items
 
-def initialize
-@items = []
-end
+  def initialize
+    @items = []
+  end
 
-def add_item(item)
-@items << item unless item_already_added?(item)
-end
+  def add_item(item)
+    @items << item unless item_already_added?(item)
+  end
 
-private
+  private
 
-def item_already_added?(item)
-@items.include?(item)
-end
+  def item_already_added?(item)
+    @items.include?(item)
+  end
 end
