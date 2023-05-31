bf9d5244b772dc16a029d3ce480a3072cc9c59ce
mumo-esther <mumoesther2021@gmail.com>
2023-05-31 13:30:17 +0300

    added a private method called item_already_added? that checks if the item is already present in the @items array

--- catalog/author.rb
+++ catalog/author.rb
@@ -1,11 +1,17 @@
 class Author
-  attr_reader :items
+attr_reader :items
 
-  def initialize
-    @items = []
-  end
+def initialize
+@items = []
+end
 
-  def add_item(item)
-    @items << item unless @items.include?(item)
-  end
+def add_item(item)
+@items << item unless item_already_added?(item)
+end
+
+private
+
+def item_already_added?(item)
+@items.include?(item)
+end
 end
