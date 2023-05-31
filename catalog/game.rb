5e897d0634c990faf7ebad609cbbbe364c2e0f9e
BB-Simon <simon.chowdery@gmail.com>
2023-05-31 14:21:47 +0300

    Override can_be_archived method

--- catalog/game.rb
+++ catalog/game.rb
@@ -1,11 +1,15 @@
 require_relative 'item'
 
 class Game < Item
-  attr_accessor :multiplayer, :last_played_at
+  attr_accessor :multiplayer, :last_played_at, :genre, :title, :publish_date
 
-  def initialize(multiplayer, last_played_at)
-    super()
+  def initialize(multiplayer, last_played_at, genre, publish_date)
+    super(genre, author, label, publish_date)
     @multiplayer = multiplayer
     @last_played_at = last_played_at
+  end
+
+  def can_be_archived?
+    super && last_played_at > 2
   end
 end
