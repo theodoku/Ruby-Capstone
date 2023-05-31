dd57c1c4471aa46740f941c21bfc01f32f318a36
mumo-esther <mumoesther2021@gmail.com>
2023-05-31 16:52:49 +0300

    Fix linter errors

--- catalog/book.rb
+++ catalog/book.rb
@@ -1,13 +1,12 @@
 class Book
-    attr_accessor :title, :author, :publisher
-  
-    def initialize(cover_state, publisher)
-      @cover_state = cover_state
-      @publisher = publisher
-    end
+  attr_accessor :title, :author, :publisher
 
-    def can_be_archived?
-      super || @cover_state == "bad"
-    end
+  def initialize(cover_state, publisher)
+    @cover_state = cover_state
+    @publisher = publisher
   end
-  
+
+  def can_be_archived?
+    super || @cover_state == 'bad'
+  end
+end
