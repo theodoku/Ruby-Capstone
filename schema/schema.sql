32a154c4806c85701338273645c127758a9de9f2
BB-Simon <simon.chowdery@gmail.com>
2023-06-01 11:38:53 +0300

    Move book and label schemas to schema file

--- schema/schema.sql
+++ schema/schema.sql
@@ -37,3 +37,20 @@ CREATE TABLE authors (
   item_id BIGINT NOT NULL,
   FOREIGN KEY (item_id) REFERENCES items(id)
 );
+
+-- Create the books table
+CREATE TABLE books (
+  id SERIAL PRIMARY KEY,
+  title VARCHAR(255) NOT NULL,
+  author VARCHAR(255) NOT NULL,
+  publisher VARCHAR(255) NOT NULL,
+  cover_state VARCHAR(255) NOT NULL
+);
+
+-- Create the labels table
+CREATE TABLE labels (
+  id SERIAL PRIMARY KEY,
+  title VARCHAR(255) NOT NULL,
+  color VARCHAR(255) NOT NULL,
+  item_id INTEGER REFERENCES items(id)
+);
