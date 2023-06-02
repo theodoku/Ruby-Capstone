cb925ebc9b2513bb02fc8d58dc419e9844edfc23
theophilus doku <theodoku89@gmail.com>
2023-06-02 10:20:42 +0000

    Modify music_album.rb file

--- catalog/music_album.rb
+++ catalog/music_album.rb
@@ -1,47 +1,39 @@
 require 'json'
 require_relative 'item'
-
+require_relative 'genre'
 class MusicAlbum < Item
-  attr_accessor :on_spotify, :genre, :title, :publish_date
-
+  attr_reader :genre
+  attr_accessor :on_spotify, :title, :publish_date
   def initialize(title, on_spotify, genre, publish_date)
-    super(genre, author, label, publish_date)
+    super(publish_date)
     @on_spotify = on_spotify
     @genre = genre
     @publish_date = publish_date
     @title = title
   end
-
   def can_be_archived?
     return true if super && @on_spotify == true
-
     false
   end
-
   def self.file_path
     './data/music_albums.json'
   end
-
   def self.load_all
     return [] unless File.exist?(file_path)
-
     file_content = File.read(file_path)
     albums_data = JSON.parse(file_content)
-    albums_data.map { |album_data| MusicAlbum.new(*album_data.values) }
+    albums_data.map { |data| MusicAlbum.new(data['title'], data['on_spotify'], Genre.new(data['genre']), data['publish_date'])}
   end
-
   def self.save_all(music_albums)
     return unless music_albums.any?
-
     data = music_albums.map do |album|
       {
         title: album.title,
         on_spotify: album.on_spotify,
-        genre: album.genre,
+        genre: album.genre.name,
         publish_date: album.publish_date
       }
     end
-
     File.write(file_path, JSON.pretty_generate(data))
   end
 end
